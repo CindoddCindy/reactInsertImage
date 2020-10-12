60428e7fd2c4168f0f0331532072365c1d31c64f
CindoddCindy <cindodcindy@gmail.com>
2020-10-12 14:23:27 +0700

    gambar

---
+++ insertImages/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ImagesExample from './ImageExample.js'
+
+const App = () => {
+   return (
+      <ImagesExample />
+   )
+}
+export default App
---
+++ insertImages/ImageExample.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+
+const ImagesExample = () => (
+   <Image source = {require('./gambar/aboutreact.png')} />
+)
+export default ImagesExample
